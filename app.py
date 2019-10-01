5d443fa9ed5d4537eeee77e8867f90009aafad66
Mark Findley <mark_findley@hotmail.com>
2019-10-01 07:47:56 -0500

    Last Revision

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 
 from flask import Flask, jsonify
 
@@ -35,14 +36,14 @@ app = Flask(__name__)
 def index():
     # List all routes that are available.
     return (
-           f"Here are the available routes:<br/>"
-           f"/api/v1.0/precipitation<br/>"
-           f"/api/v1.0/stations<br/>"
-           f"/api/v1.0/tobs<br/>"
-           f"/api/v1.0/averagetempsbystartdate<br/>"
-           f"/api/v1.0/averagetempsbystartandenddate"
+        f"Here are the available routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
     )
     
+
 # Convert the query results to a Dictionary using date as the key and prcp as the value.
 # Return the JSON representation of your dictionary.
 @app.route("/api/v1.0/precipitation")
@@ -52,8 +53,11 @@ def precipitation():
     session = Session(engine)
 
     """Return a list of all precipitation measurements"""
+    prev_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
     # Query all measurements
-    results = session.query(Measurement.date, Measurement.prcp).all()
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_date).all()
 
     session.close()
 
@@ -61,7 +65,7 @@ def precipitation():
     all_precipitation = []
     for date, prcp in results:
         precipitation_dict = {}
-        precipitation_dict["date"] = date
+        precipitation_dict["date:"] = date
         precipitation_dict["precipitation"] = prcp
         all_precipitation.append(precipitation_dict)
 
@@ -84,7 +88,7 @@ def stations():
     all_stations = []
     for station, name in results:
         station_dict = {}
-        station_dict["station"] = station
+        station_dict["station:"] = station
         station_dict["station name"] = name
         all_stations.append(station_dict)
 
@@ -98,17 +102,18 @@ def tobs():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    prev_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)    
+
     results = session.query(Measurement.station, Measurement.date, Measurement.tobs).\
         filter(Measurement.station == 'USC00519281').\
-        filter(Measurement.date >= '2016-08-23').\
-        filter(Measurement.date <= '2017-08-23').all()
+        filter(Measurement.date >= prev_date).all()
 
     session.close()  
 
     all_tobs = []
     for station, date, tobs in results:
         tobs_dict = {}
-        tobs_dict["station"] = station
+        tobs_dict["station:"] = station
         tobs_dict["date"] = date
         tobs_dict["tobs"] = tobs
         all_tobs.append(tobs_dict)
@@ -117,19 +122,20 @@ def tobs():
 
 # # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 # # When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
-@app.route("/api/v1.0/averagetempsbystartdate/<date_string>")
-def start(date_string):
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def temps(start=None, end=None):
 
     session = Session(engine)
 
     min_temp = session.query(min(Measurement.tobs)).\
-        filter(Measurement.date >= 'date_string').all()
+        filter(Measurement.date >= start).all()
 
     max_temp = session.query(max(Measurement.tobs)).\
-        filter(Measurement.date >= 'date_string').all()
+        filter(Measurement.date >= start).all()
 
     # avg_temp = session.query(avg(Measurement.tobs)).\
-    #     filter(Measurement.date >= 'date_string').all()                
+    #     filter(Measurement.date >= start).all()                
 
     session.close()      
 
@@ -139,22 +145,7 @@ def start(date_string):
            f"The maximumum temperature is: {max_temp}<br/>"
         #    f"The average temperature is: {avg_temp}<br/>"
     )
-
-def to_date(date_string): 
-    try:
-        return datetime.datetime.strptime(dateString, "%Y-%m-%d").date()
-    except ValueError:
-        raise ValueError('{} is not valid date in the format YYYY-MM-DD'.format(date_string))
-
-@app.route()
-def event():
-    try:
-        ektempo = to_date(request.args.get('start', default = datetime.date.today().isoformat()))
-    except ValueError as ex:
-        return jsonify({'error': str(ex)}), 400   # jsonify, if this is a json api    
        
-# engine.execute('select avg(tobs), min(tobs), max(tobs) from Measurement where date >= "2017-08-23"').fetchall()
-# [(80.25, 76.0, 82.0)]       
 
 # # When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
 # @app.route("/api/v1.0/averagetempsbystartandenddate")
